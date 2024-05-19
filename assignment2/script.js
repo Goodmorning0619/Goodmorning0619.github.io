4fef2b06f84ac39b05d5776842f280d018d02430
Goodmorning0619 <yin9101@gmail.com>
2024-05-20 07:45:04 +1000

    add

--- assignment2/script.js
+++ assignment2/script.js
@@ -2,6 +2,8 @@ const video = document.querySelector("#custom-video-player");
 const playPauseBtn = document.querySelector("#play-pause-btn");
 const playPauseImg = document.querySelector("#play-pause-img");
 const progressBar = document.querySelector("#progress-bar-fill");
+const popSound = document.querySelector("#pop-sound");
+console.log(popSound);
 video.removeAttribute("controls");
 // playPauseBtn.addEventListener("click", togglePlayPause);
 video.addEventListener("timeupdate", updateProgressBar);
@@ -14,8 +16,41 @@ function togglePlayPause() {
     playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
   }
 }
+
+function popIt() {
+  popSound.play();
+}
+
 function updateProgressBar() {
   const value = (video.currentTime / video.duration) * 100;
   progressBar.style.width = value + "%";
 }
 // Add other functionalities here
+
+const muteUnmuteButton = document.querySelector("#mute-unmute-button");
+console.log(muteUnmuteButton);
+muteUnmuteButton.addEventListener("click", toggleAudio);
+const muteUnmuteImg = document.querySelector("mute-unmute-img");
+
+function toggleAudio() {
+  if (myVideo.muted) {
+    muteUnmuteButton.style.backgroundColor = "red";
+    myVideo.muted = false;
+  } else {
+    muteUnmuteButton.style.backgroundColor = "blue";
+    myVideo.muted = true;
+  }
+}
+
+let likeCount = 0;
+
+const likeButton = document.querySelector("#like-button");
+console.log(likeButton);
+likeButton.addEventListener("click", addLikes);
+const likes = document.querySelector("#likes");
+likes.textContent = likeCount;
+
+function addLikes() {
+  likeCount++;
+  likes.textContent = likeCount;
+}
