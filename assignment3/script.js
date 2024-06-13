479a60d6f1e7f9c353315dcfdbfbf715b6a6e842
Goodmorning0619 <yin9101@gmail.com>
2024-06-14 07:18:00 +1000

    add

--- assignment3/script.js
+++ assignment3/script.js
@@ -1,1 +1,11 @@
+const clickButton = document.querySelector("#click-button");
+console.log(clickButton);
 
+const clickSound = document.querySelector("#click-sound");
+console.log(clickSound);
+
+clickButton.addEventListener("click", popIt);
+
+function popIt() {
+  clickSound.play();
+}
